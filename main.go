8678a4ccfed098a15a04c17dd9f9a8e7ddd5c398
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-12 15:48:41 +0200

    Add the learner basic struct

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type learner struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Class   string `json:"class"`
+	Average string `json:"average"`
+}
+
 func main() {
 	fmt.Println("vim-go")
 }
