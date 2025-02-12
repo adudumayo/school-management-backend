689cb2bdaefcf14dee237b71d8fb0a0f1d8cb9cb
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-12 16:10:55 +0200

    Add a basic learners slice

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ type learner struct {
 	Average float64 `json:"average"`
 }
 
+var learners = []learner{
+	{ID: 1, Name: "Learner One", Grade: 8, Average: 34.66},
+	{ID: 2, Name: "Learner Two", Grade: 10, Average: 60.89},
+	{ID: 3, Name: "Learner Three", Grade: 9, Average: 79.23},
+}
+
 func main() {
 	fmt.Println("vim-go")
 }
