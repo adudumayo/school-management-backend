d63cedcf95a3174e51d856112a60ef2f43ab576a
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-12 16:28:36 +0200

    Add the getLearners function to return the slice

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type learner struct {
 	ID      int     `json:"id"`
@@ -13,6 +16,10 @@ var learners = []learner{
 	{ID: 1, Name: "Learner One", Grade: 8, Average: 34.66},
 	{ID: 2, Name: "Learner Two", Grade: 10, Average: 60.89},
 	{ID: 3, Name: "Learner Three", Grade: 9, Average: 79.23},
+}
+
+func getLearners(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, learners)
 }
 
 func main() {
