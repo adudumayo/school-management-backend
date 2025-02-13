e880035b13dcd5530eba350b365cf83ea2c7a453
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-13 21:14:36 +0200

    Change Grade in learner struct to Class

--- backend/main.go
+++ backend/main.go
@@ -10,14 +10,14 @@ import (
 type learner struct {
 	ID      int     `json:"id"`
 	Name    string  `json:"name"`
-	Grade   int     `json:"class"`
+	Class   int     `json:"class"`
 	Average float64 `json:"average"`
 }
 
 var learners = []learner{
-	{ID: 1, Name: "Learner One", Grade: 8, Average: 34.66},
-	{ID: 2, Name: "Learner Two", Grade: 10, Average: 60.89},
-	{ID: 3, Name: "Learner Three", Grade: 9, Average: 79.23},
+	{ID: 1, Name: "Learner One", Class: 8, Average: 34.66},
+	{ID: 2, Name: "Learner Two", Class: 10, Average: 60.89},
+	{ID: 3, Name: "Learner Three", Class: 9, Average: 79.23},
 }
 
 func getLearners(c *gin.Context) {
