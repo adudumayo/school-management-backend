2dce66901a82165e178f90ff2a4828369548eed3
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-15 20:51:17 +0200

    Insert learners to the learner table using the script

--- internal/db/create_learners.sql
+++ internal/db/create_learners.sql
@@ -7,3 +7,11 @@ CREATE TABLE learner(
     average DECIMAL(5,2) NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+INSERT INTO learner (name, class, average) VALUES
+('Sipho Ndlovu', 10, 75.50),
+('Ayanda Dlamini', 11, 82.30),
+('Lebo Mokoena', 9, 90.10),
+('Thabo Khumalo', 12, 88.75),
+('Zanele Nkosi', 10, 79.20);
+
