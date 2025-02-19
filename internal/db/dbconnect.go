4eef967f54fb0f78337192a6337ada0afd6dd88d
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-19 20:11:15 +0200

    Add the db connect code

--- internal/db/dbconnect.go
+++ internal/db/dbconnect.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
 
-func main() {
-	fmt.Println("vim-go")
+var db *sql.DB
+
+// Connect to MySQL database
+func connectDB() {
+	var err error
+	dbUser := os.Getenv("DBUSER")
+	dbPass := os.Getenv("DBPASS")
+	dbName := os.Getenv("DBNAME")
+	dbHost := os.Getenv("DBHOST")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbName)
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Database unreachable:", err)
+	}
+
+	fmt.Println("Connected to the database!")
 }
