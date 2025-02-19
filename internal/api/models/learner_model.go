af42b0a06efdcc93bfe6233707bc55e3bc1a7908
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-19 20:05:25 +0200

    Add dir for models and have a dedicated dbconnect code

---
+++ internal/api/models/learner_model.go
@@ -0,0 +1,9 @@
+package main
+
+// Learner model
+type Learner struct {
+	ID      int     `json:"id"`
+	Name    string  `json:"name"`
+	Class   int     `json:"class"`
+	Average float64 `json:"average"`
+}
