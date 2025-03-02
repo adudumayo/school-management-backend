f2cdfdbe556c3d8aeeb2bab3f867440d75a79711
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-03-02 19:03:26 +0200

    Remove the connect db func from main and call it from model package

--- main.go
+++ main.go
@@ -3,38 +3,16 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 
+	"github.com/adudumayo/school-management-backend/model"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
-
-// Connect to MySQL database
-func connectDB() {
-	var err error
-	dbUser := os.Getenv("DBUSER")
-	dbPass := os.Getenv("DBPASS")
-	dbName := os.Getenv("DBNAME")
-	dbHost := os.Getenv("DBHOST")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbName)
-	db, err = sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal("Database connection failed:", err)
-	}
-
-	if err = db.Ping(); err != nil {
-		log.Fatal("Database unreachable:", err)
-	}
-
-	fmt.Println("Connected to the database!")
-}
 
 // Learner model
 type Learner struct {
@@ -134,7 +112,7 @@ func removeLearnerByID(c *gin.Context) {
 
 // Main function
 func main() {
-	connectDB()
+	db = model.ConnectDB()
 
 	router := gin.Default()
 	router.Use(cors.Default())
