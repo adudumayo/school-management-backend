36a15355443d9d2a5adb32e22a729861465cb611
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-03-06 16:54:56 +0200

    Change the sql driver db to be a global var DB

--- model/connect.go
+++ model/connect.go
@@ -10,19 +10,19 @@ import (
 // Connect to MySQL database
 func ConnectDB() *sql.DB {
 	var err error
-	var db *sql.DB
+	var DB *sql.DB
 	dbUser := os.Getenv("DBUSER")
 	dbPass := os.Getenv("DBPASS")
 	dbName := os.Getenv("DBNAME")
 	dbHost := os.Getenv("DBHOST")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbName)
-	db, err = sql.Open("mysql", dsn)
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Database connection failed:", err)
 	}
 
-	if err = db.Ping(); err != nil {
+	if err = DB.Ping(); err != nil {
 		log.Fatal("Database unreachable:", err)
 	}
 
