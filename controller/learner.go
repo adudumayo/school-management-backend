1bdda5ff7c3725f593b6f2deead7e55ac8cef22d
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-03-27 16:32:34 +0200

    Add the endpoint handler to get all quizzes as a learner

---
+++ controller/learner.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/adudumayo/school-management-backend/model"
+	"github.com/adudumayo/school-management-backend/view"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Get all quizzes
+func GetQuizzes(c *gin.Context) {
+	rows, err := model.DB.Query("SELECT id, subject, topic, question, due_date FROM quiz")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch quizzes"})
+		return
+	}
+	defer rows.Close()
+
+	var quizzes []view.Quiz
+	for rows.Next() {
+		var q view.Quiz
+		if err := rows.Scan(&q.ID, &q.Subject, &q.Topic, &q.Question, &q.Due_date); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error parsing quiz data"})
+			return
+		}
+		quizzes = append(quizzes, q)
+	}
+
+	c.JSON(http.StatusOK, quizzes)
+}
