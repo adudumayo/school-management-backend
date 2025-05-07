97d11ce0aff7ed13d0668e38d94744d202cae17d
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-05-07 18:21:46 +0200

    Add the mock teacher sql script so I always have teachers on the database-idk what happened to the last commit

---
+++ model/mock_teachers.sql
@@ -0,0 +1,7 @@
+INSERT INTO teacher (surname, username, title, password)
+VALUES 
+  ('Mthembu', 'smthembu', 'Mr', 'pass123'),
+  ('Khumalo', 'kkhumalo', 'Mrs', 'letmein'),
+  ('Dlamini', 'tdlamini', 'Dr', 'abc123'),
+  ('Zulu', 'mzulu', 'Prof', 'qwerty'),
+  ('Ngcobo', 'sngcobo', 'Ms', '123456');
